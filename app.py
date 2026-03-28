36fd58951510e7b3d5f9929c99ab9952c677c4da
Stefany Medellin <sg.medellingonzalez@ugto.mx>
2026-03-28 16:37:08 -0600

    implementando una app basica de Streanlint para mostrar un historial (proyecto base)

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+car_data = pd.read_csv(
+    '/Users/stef/Desktop/TripleTen/Proyecto_web/vehicles_us/vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
